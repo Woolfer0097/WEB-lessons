e76cdc908ff9a41e7bf08291e35dd7063f168874
Woolfer0097 <Woolfer0097@yandex.ru>
2021-03-30 18:26:37 +0300

    30.03.2021

--- WEB. Знакомство с flask-sqlalchemy/Добавляем капитана/data/users.py
+++ WEB. Знакомство с flask-sqlalchemy/Добавляем капитана/data/users.py
@@ -1,17 +1,18 @@
 import datetime
 import sqlalchemy
-from db_session import SqlAlchemyBase
+from .db_session import SqlAlchemyBase
 
 
 class User(SqlAlchemyBase):
     __tablename__ = 'users'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer,
-                           primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    email = sqlalchemy.Column(sqlalchemy.String,
-                              index=True, unique=True, nullable=True)
-    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
-                                     default=datetime.datetime.now)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    age = sqlalchemy.Column(sqlalchemy.Integer)
+    position = sqlalchemy.Column(sqlalchemy.String)
+    speciality = sqlalchemy.Column(sqlalchemy.String)
+    address = sqlalchemy.Column(sqlalchemy.String)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
--- WEB. Знакомство с flask-sqlalchemy/Добавляем капитана/main.py
+++ WEB. Знакомство с flask-sqlalchemy/Добавляем капитана/main.py
@@ -1,12 +1,29 @@
 from flask import Flask
 from data import db_session
+from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def main():
-    app.run()
+    db_session.global_init("db/blogs.db")
+    info = [["Scott", "Ridley", 21, "captain", "research engineer", "module_1", "scott_chief@mars.org"],
+            ["Jack", "PewDiePie", 35, "corporal", "programmer", "module_2", "Jack_PewDiePie@mars.org"],
+            ["Jack", "Levi", 34, "corporal", "strategist", "module_3", "Jack_Levi@marx.org"]]
+    for surname, name, age, position, speciality, address, email in info:
+        user = User()
+        user.surname = surname
+        user.name = name
+        user.age = age
+        user.position = position
+        user.speciality = speciality
+        user.address = address
+        user.email = email
+        db_sess = db_session.create_session()
+        db_sess.add(user)
+        db_sess.commit()
+    # app.run()
 
 
 if __name__ == '__main__':
