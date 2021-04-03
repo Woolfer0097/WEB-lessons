fb94d6fd2d55d27e74486647445d058ce4ff9185
Woolfer0097 <Woolfer0097@yandex.ru>
2021-04-04 00:11:22 +0300

    04.04.2021 1

---
+++ WEB. REST-API. Понятие. Делаем простое Rest-api/Получение всех работ/data/jobs_api.py
@@ -0,0 +1,15 @@
+import flask
+
+from . import db_session
+from .jobs import Jobs
+
+blueprint = flask.Blueprint(
+    'jobs_api',
+    __name__,
+    template_folder='templates'
+)
+
+
+@blueprint.route('/api/jobs')
+def get_news():
+    return "Обработчик в jobs_api"
