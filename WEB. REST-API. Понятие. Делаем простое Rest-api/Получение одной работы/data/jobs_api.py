e25d7cc5adef3fbbf88863cc1b71938ddadd64ad
Woolfer0097 <Woolfer0097@yandex.ru>
2021-04-04 01:19:22 +0300

    04.04.2021 2

---
+++ WEB. REST-API. Понятие. Делаем простое Rest-api/Получение одной работы/data/jobs_api.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, jsonify
+
+from . import db_session
+from .jobs import Jobs
+
+blueprint = Blueprint(
+    'jobs_api',
+    __name__,
+    template_folder='templates'
+)
+
+
+@blueprint.route('/api/jobs/<int:jobs_id>', methods=['GET'])
+def get_one_news(jobs_id):
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).get(jobs_id)
+    if not jobs:
+        return jsonify({'error': 'Not found'})
+    return jsonify(
+        {
+            'jobs': jobs.job
+        }
+    )
