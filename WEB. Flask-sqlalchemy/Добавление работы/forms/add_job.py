77730de67e8a780e7fd4b6cc3e6c6480e0e335be
Woolfer0097 <Woolfer0097@yandex.ru>
2021-04-07 17:24:09 +0300

    07.04.2021

---
+++ WEB. Flask-sqlalchemy/Добавление работы/forms/add_job.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, SubmitField, StringField, IntegerField
+from wtforms.validators import DataRequired
+
+
+class AddJobForm(FlaskForm):
+    team_leader = IntegerField('Лидер команды', validators=[DataRequired()])
+    job = StringField('Работа', validators=[DataRequired()])
+    work_size = IntegerField('Длина работы', validators=[DataRequired()])
+    collaborators = StringField('Члены команды', validators=[DataRequired()])
+    is_finished = BooleanField('Работа завершена')
+    submit = SubmitField('Добавить')
