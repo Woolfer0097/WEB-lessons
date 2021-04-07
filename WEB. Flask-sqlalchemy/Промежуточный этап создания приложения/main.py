581d9c9a54f365517aee3f0b123236d1a8e22523
Woolfer0097 <Woolfer0097@yandex.ru>
2021-04-07 22:44:12 +0300

    07.04.2021

---
+++ WEB. Flask-sqlalchemy/Промежуточный этап создания приложения/main.py
@@ -0,0 +1,107 @@
+from flask import Flask, render_template, redirect
+from data import db_session
+from data.users import User
+from data.jobs import Jobs
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
+from forms.login import LoginForm
+from forms.register import RegisterForm
+from forms.add_job import AddJobForm
+
+app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template("login.html", title="Авторизация", form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    db_sess = db_session.create_session()
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            email=form.email.data,
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect("/login")
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route("/")
+def main_page():
+    db_sess = db_session.create_session()
+    if current_user.is_authenticated:
+        user = db_sess.query(User).filter(User.id == current_user.id).first()
+        jobs = db_sess.query(Jobs).filter(Jobs.team_leader == current_user.id).all()
+        user_name = f"{user.name} {user.surname}"
+        keys = ['team_leader', 'job', 'work_size', 'collaborators', 'is_finished']
+        values = [[user_name, job.job, job.work_size, job.collaborators, job.is_finished] for job in jobs]
+        return render_template("main_page.html", title="Главная страница", name=user_name, keys=keys, data=values)
+    return render_template("main_page.html", title="Главная страница")
+
+
+@app.route("/add_job", methods=["GET", "POST"])
+@login_required
+def add_job():
+    form = AddJobForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        jobs = Jobs()
+        jobs.team_leader = form.team_leader.data
+        jobs.job = form.job.data
+        jobs.work_size = form.work_size.data
+        jobs.collaborators = form.collaborators.data
+        jobs.is_finished = form.is_finished.data
+        current_user.jobs.append(jobs)
+        db_sess.merge(current_user)
+        db_sess.commit()
+        return redirect("/")
+    return render_template("add_job.html", title="Добавление работы", form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+if __name__ == '__main__':
+    db_session.global_init("db/blogs.db")
+    app.run(port=8080, host='127.0.0.1')
